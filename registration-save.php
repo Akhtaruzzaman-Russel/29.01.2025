5154e1fb703316dc3e659030fa02a69066692bf9
KaziRussel <kaziakhtaruzzaman22@gmail.com>
2025-02-05 12:51:33 +0600

    050220251251pm

--- registration-save.php
+++ registration-save.php
@@ -4,10 +4,46 @@ $useremail=$_POST['email'];
 $userpassword=$_POST['password'];
 
 
-echo $username;
+// echo $username;
 
 
+$conn=mysqli_connect("localhost","root","","db");
 
+/*
+if($conn==true){
+    echo "Connected successfully";
+}else{
+    echo "Connection failed" ;
+}
+*/
+/*
+if($conn==true){
+    // echo "Connected successfully";
+    // $query="INSERT INTO users (username, email, password) VALUES ('$username', '$useremail', '$userpassword')";
+    $query="INSERT INTO `client`( `user`, `email`, `password`) VALUES ('zaman','admin@admin.com','123456')";
+    mysqli_query($conn,$query);
+}else{
+    echo "Connection failed" ;
+}
+*/
+
+if($conn==true){
+    // echo "Connected successfully";
+    // $query="INSERT INTO users (username, email, password) VALUES ('$username', '$useremail', '$userpassword')";
+    $query="INSERT INTO `client`( `user`, `email`, `password`) VALUES ('$username','$useremail','$userpassword')";
+   $sql= mysqli_query($conn,$query);
+
+   if($sql){
+    echo "New record created successfully";
+   }else{
+    echo"SOrry ghure aay";
+   }
+}else{
+    echo "Connection failed" ;
+}
+
+
+// mysqli_query("INSERT INTO users (username, email, password) VALUES ('$username', '$useremail', '$userpassword')");
 
 
 ?>
